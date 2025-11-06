58bb78dc4a1955eb086a715fadfb5801f13fd8e3
Mois Khan <moiskhanmd9090@gmail.com>
2025-11-06 19:53:34 +0530

    formated the app.py with comments

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
-import whisper
+import whisper 
 import os
 
 app = Flask(__name__)
 
+# Create a temporary folder to store uploaded files
 UPLOAD_FOLDER = '../tmp_folder/'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -15,6 +16,7 @@ def home():
 @app.route('/transcribe', methods=["POST"])
 def transcribe():
 
+    # Check if a file is present in the request
     if "file" not in request.files:
         return {"status": 400, "message": "No file uploaded"}
     
@@ -23,19 +25,20 @@ def transcribe():
     if file.filename == "":
         return {"status": 400, "message": "No file selected"}
 
+    # Save the uploaded file to the temporary folder
     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(file_path)
-
     print("File received....")
 
+    # Transcription logic using Whisper
     print("Transcribing....")
-
     model = whisper.load_model("base")
     result = model.transcribe(file_path)
 
     print("Transcription done..")
-    print(result["text"])
+    print(f"\n{result['text']}")
 
+    # Clean up the uploaded file after transcription
     os.remove(file_path)
 
     return {"status": 200, "message": "Transcription successful", "data": result["text"]}
